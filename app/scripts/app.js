2fd34666436e5c8f45ba0d3483df8fc27dc23c83
Yuri Reyes <reyesyuri@yahoo.com>
2016-10-06 12:17:57 -0700

    Checkpoint 5 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,6 +10,7 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
         
@@ -20,6 +21,7 @@
     
             .state('collection', {
                 url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             });
     
