cee8fbc8989b0f52a5399292b25d2f59b8a2df12
Yuri Reyes <reyesyuri@yahoo.com>
2016-10-06 15:01:41 -0700

    Checkpoint 6 complete

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,12 +1,12 @@
 (function() {
-     function CollectionCtrl() {
+     function CollectionCtrl(Fixtures) {
         this.albums = [];
         for (var i=0; i < 12; i++) {
-            this.albums.push(angular.copy(albumPicasso));
+            this.albums.push(Fixtures.getAlbum());
         }
      }
  
      angular
          .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
+         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
 })();
