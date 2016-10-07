c06bc92dc9e26440bafcc815dc6d91009ca1d7ed
Yuri Reyes <reyesyuri@yahoo.com>
2016-10-06 21:40:06 -0700

    Assignment 6 complete

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,9 +1,6 @@
 (function() {
      function CollectionCtrl(Fixtures) {
-        this.albums = [];
-        for (var i=0; i < 12; i++) {
-            this.albums.push(Fixtures.getAlbum());
-        }
+        this.albums = Fixtures.getCollection(12);
      }
  
      angular
--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,14 @@
             return albumPicasso;
         };
          
+        Fixtures.getCollection = function(numberOfAlbums) {
+            var albums = [];
+            for (var i = 0; i < numberOfAlbums; i++) {
+                albums.push(angular.copy(albumPicasso));
+            }
+            return albums;
+    };
+         
         return Fixtures;
     }
  
