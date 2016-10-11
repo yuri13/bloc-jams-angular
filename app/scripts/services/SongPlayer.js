e9256efd6e05da0bd2c59e4561fb9cea4c134d11
Yuri Reyes <reyesyuri@yahoo.com>
2016-10-10 20:49:25 -0700

    Checkpoint 10 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
         
         /**
@@ -31,7 +31,14 @@
             formats: ['mp3'],
             preload: true
             });
- 
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+            
+        // example has SongPlayer.currentSong = song; 
             currentSong = song;
         };
         
@@ -51,6 +58,12 @@
         */ 
          
         SongPlayer.currentSong = null;
+         
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
          
         /**
         * @function playSong
@@ -142,10 +155,21 @@
             }            
         };
          
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+         
         return SongPlayer;
 }
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
